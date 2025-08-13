e8276d94ae697f18ceb999ebc6e97b7e588a94c2
Francisco Rocha <fa676533@gmail.com>
2025-08-12 22:35:26 -0600

    api creada

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,6 @@ export default defineConfig({
         port: 5173,
         open: true,
         cors: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                rewrite: path => path.replace(/^\/api/, '')
-            }
-        }
     },
     resolve: {
         alias: {
